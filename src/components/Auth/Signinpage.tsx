aabc45583149029164b2fe14c5c568a5dc90cc27
yash gupta <141262414+yashtech00@users.noreply.github.com>
2025-03-02 00:25:58 +0530

    update

---
+++ src/components/Auth/Signinpage.tsx
@@ -0,0 +1,9 @@
+
+
+export const SigninPage = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/components/Auth/SignupPage.tsx
@@ -0,0 +1,10 @@
+
+
+
+export const SignInPage=()=>{
+    return (
+        <div>
+            
+        </div>
+    )
+}
