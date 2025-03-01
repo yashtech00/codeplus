e1d20f54dc3643ee973a3f9d7e781df3dc8dc120
yash gupta <141262414+yashtech00@users.noreply.github.com>
2025-03-02 02:22:36 +0530

    update

--- src/components/AppBar/Appbar.tsx
+++ src/components/AppBar/Appbar.tsx
@@ -8,11 +8,13 @@ export const AppBar = () => {
         { name: 'Home', url: '#', icon: Home },
         { name: 'About', url: '#', icon: User },
         { name: 'Projects', url: '#', icon: Briefcase },
-        { name: 'Resume', url: '#', icon: FileText }
+        { name: 'Resume', url: '#', icon: FileText },
+        { name: 'Login', url: '#', icon: FileText },
+        { name: 'Signup', url: '#', icon: FileText }
       ]
     return (
         <div>
-            <div className=' text-white'>hello</div>
+            
         <NavBar items={navItems}/>
         </div>
     )
--- src/components/ui/tubelight-navbar.tsx
+++ src/components/ui/tubelight-navbar.tsx
@@ -38,7 +38,7 @@ export function NavBar({ items, className }: NavBarProps) {
         className,
       )}
     >
-      <div className="flex items-center gap-3 bg-background/5 border border-border backdrop-blur-lg py-1 px-1 rounded-full shadow-lg">
+      <div className="flex items-center gap-3 bg-black border border-black  backdrop-blur-lg py-1 px-1 rounded-full shadow-lg">
         {items.map((item) => {
           const Icon = item.icon
           const isActive = activeTab === item.name
@@ -50,7 +50,7 @@ export function NavBar({ items, className }: NavBarProps) {
               onClick={() => setActiveTab(item.name)}
               className={cn(
                 "relative cursor-pointer text-sm font-semibold px-6 py-2 rounded-full transition-colors",
-                "text-foreground/80 hover:text-primary",
+                "text-white hover:text-primary",
                 isActive && "bg-muted text-primary",
               )}
             >
@@ -69,7 +69,7 @@ export function NavBar({ items, className }: NavBarProps) {
                     damping: 30,
                   }}
                 >
-                  <div className="absolute -top-2 left-1/2 -translate-x-1/2 w-8 h-1 bg-primary rounded-t-full">
+                  <div className="absolute -top-2 left-1/2 -translate-x-1/2 w-8 h-1 bg-white rounded-t-full">
                     <div className="absolute w-12 h-6 bg-primary/20 rounded-full blur-md -top-2 -left-2" />
                     <div className="absolute w-8 h-6 bg-primary/20 rounded-full blur-md -top-1" />
                     <div className="absolute w-4 h-4 bg-primary/20 rounded-full blur-sm top-0 left-2" />
