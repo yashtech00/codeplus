e6589353c03e897474e64cea8d584ff8e8a14a6d
yash gupta <141262414+yashtech00@users.noreply.github.com>
2025-03-01 20:15:33 +0530

    update

--- src/lib/authoptions.ts
+++ src/lib/authoptions.ts
@@ -1,3 +1,123 @@
 
+import CredentialsProvider from "next-auth/providers/credentials";
+import GithubProvider from "next-auth/providers/github";
+import prisma from "./db";
+import { emailSchema, passwordSchema } from "../Schema/utils";
+import bcrypt from "bcryptjs";
+import { NextAuthOptions,Session } from "next-auth";
 
-const authoptions = () 
+import { JWT } from "next-auth/jwt";
+
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.Github_Id || "",
+      clientSecret: process.env.Github_Secret || "",
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { type: "email" },
+        password: { type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          return null;
+        }
+
+        const emailValidation = emailSchema.safeParse(credentials.email);
+        if (!emailValidation.success) {
+          throw new Error("Please enter valid email");
+        }
+        const passwordValidation = passwordSchema.safeParse(
+          credentials.password
+        );
+        if (!passwordValidation.success) {
+          throw new Error(
+            "Please enter valid password or password length more than 6"
+          );
+        }
+
+        try {
+          const user = await prisma.user.findUnique({
+            where: {
+              email: emailValidation.data,
+            },
+          });
+          if (!user) {
+            const hashedPassword = await bcrypt.hash(
+              passwordValidation.data,
+              10
+            );
+            const newUser = await prisma.user.create({
+              data: {
+                email: emailValidation.data,
+                password: hashedPassword,
+                provider: "Credentials",
+              },
+            });
+            return newUser;
+          }
+          const passwordVerification = await bcrypt.compare(
+            passwordValidation.data,
+            user.password
+          );
+          if (!passwordVerification) {
+            throw new Error("invaild password ");
+          }
+          return user;
+        } catch (e) {}
+      },
+    }),
+    ],
+    pages: {
+       signIn:"/auth"
+    },
+    session: {
+        strategy:"jwt"
+    },
+    callbacks: {
+        async jwt({token,account,profile}) {
+            if (account && profile) {
+                token.email = profile.email as string;
+                token.id = account.access_token; 
+            }
+            return token;
+        },
+        async session({session,token}:{session:Session, token:JWT}) {
+            try {
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email:token.email
+                    }
+                })
+                if (user) {
+                    session.user.id = user.id;
+                }
+            } catch (e) {
+                console.error(e);
+                
+            }
+            return session;
+        },
+        async signIn({account,profile}) {
+            try {
+
+
+                if (account?.provider === "github") {
+                    const user = await prisma.user.findUnique
+                }
+                const newUser = await prisma.user.create({
+                    data: {
+                        email: profile?.email,
+                        password: profile.password,
+                        provider:"Credentials"
+                    }
+                })
+            } catch (e) {
+                console.error(e);
+                
+            }
+        }
+    }
+}satisfies NextAuthOptions;
