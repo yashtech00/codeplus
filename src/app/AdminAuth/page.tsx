eb04a89769bc00457e3b185ec8015c1ca840c3b0
yash gupta <141262414+yashtech00@users.noreply.github.com>
2025-03-03 18:55:15 +0530

    update

---
+++ src/app/AdminAuth/page.tsx
@@ -0,0 +1,10 @@
+import { AdminSigninPage } from "@/components/Admin/AdminLogin";
+
+
+export default function AdminAuth(){
+    return (
+        <div>
+            <AdminSigninPage/>
+        </div>
+    )
+}
---
+++ src/lib/admin-auth-options.ts
@@ -0,0 +1,52 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import { NextResponse } from "next/server";
+
+
+export const AdminAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "Admin Auth",
+            credentials: {
+                email: { type: "email" },
+                password:{type:"password"}
+            },
+            async authorize(credentials){
+                if (!credentials || !credentials.email || !credentials.password) {
+                    return null;
+                }
+
+                try {
+                    const admin = await prisma.admin.create({
+                        where: {
+                            email: "sample@gmail.com",
+                            password:"1234567890"
+                        }
+                    })
+                    return NextResponse.json({
+                        message: "Login Successfully",
+                        admin
+                    }, {
+                        status:200
+                    })
+                } catch (e) {
+                    console.error(e);
+                    return NextResponse.json({
+                        message:"Internal server error",
+                    }, {
+                        status:411
+                    })
+                }
+            }
+        }),
+       
+    ],
+    pages: {
+        signIn:"/AdminAuth"
+    },
+    session: {
+        strategy: "jwt",
+        
+        maxAge: 24 * 60 * 60,
+        
+    }
+}
