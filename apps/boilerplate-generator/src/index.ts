91b2d982fe4c445588a14e32b76099b71fbce45a
yash gupta <141262414+yashtech00@users.noreply.github.com>
2025-03-07 15:49:32 +0530

    add new

---
+++ apps/boilerplate-generator/src/index.ts
@@ -0,0 +1,32 @@
+import path from 'path'
+import fs from 'fs'
+
+function generatePartialBoilerPlate(generatorFilePath:string) {
+    const inputFilePath = path.join(__dirname, generatorFilePath, "Structure.md")
+    const boilerFilePath = path.join(__dirname, generatorFilePath, "boilerPlate")
+    
+
+    const input = fs.readFileSync(inputFilePath, "utf-8");
+
+    const parser = new ProblemDefinitionParser();
+    parser.parse(input);
+
+    const cppCode = parser.generateCpp();
+    const jsCode = parser.generateJs();
+    const rsCode = parser.generateRs();
+
+    if (!fs.existsSync(boilerFilePath)) {
+        fs.mkdirSync(boilerFilePath, { recursive: true });
+    }
+
+    fs.writeFileSync(path.join(boilerFilePath, "function.cpp"), cppCode);
+    fs.writeFileSync(path.join(boilerFilePath, "function.js"), jsCode);
+    fs.writeFileSync(path.join(boilerFilePath, "function.rs"), rsCode);
+
+    console.log("Boilerplate code is generated");
+    
+}
+
+function generateFullBoilerPlate(generatorFilePath: string) {
+    const input = 
+}
