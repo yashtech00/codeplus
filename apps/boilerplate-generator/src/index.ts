f4f47db901121ca758398d58170c6c4f25733a2a
yash gupta <141262414+yashtech00@users.noreply.github.com>
2025-03-07 15:56:49 +0530

    update boiler plate

--- apps/boilerplate-generator/src/index.ts
+++ apps/boilerplate-generator/src/index.ts
@@ -28,5 +28,24 @@ function generatePartialBoilerPlate(generatorFilePath:string) {
 }
 
 function generateFullBoilerPlate(generatorFilePath: string) {
-    const input = 
+    const inputFilePath = path.join(__dirname, generatorFilePath, "Structure.md");
+    const boilerFilePath = path.join(__dirname, generatorFilePath, "boilerPlate-Full");
+
+    const input = fs.readFileSync(inputFilePath, "utf-8");
+    const parser = new FulldefinitionParser();
+    parser.parse(input);
+
+    const cppCode = parser.generateCpp();
+    const jsCode = parser.generateJs();
+    const rsCode = parser.generateRs();
+
+    if (!fs.existsSync(boilerFilePath)) {
+        fs.mkdirSync(boilerFilePath, { recursive: true });
+    }
+
+    fs.writeFileSync(path.join(boilerFilePath, "function.cpp"), cppCode);
+    fs.writeFileSync(path.join(boilerFilePath, "function.js"), jsCode);
+    fs.writeFileSync(path.join(boilerFilePath, "function.rs"), rsCode);
+    console.log("Full BoilerPlate code is generated successfully");
+    
 }
