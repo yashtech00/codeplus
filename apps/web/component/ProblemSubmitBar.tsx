eb5ba686e1b68dbf874e5212ae075e676f1ee564
yash gupta <141262414+yashtech00@users.noreply.github.com>
2025-03-09 10:57:10 +0530

    feat: implement ProblemSubmitBar component with Monaco Editor and tabs

--- apps/web/component/ProblemSubmitBar.tsx
+++ apps/web/component/ProblemSubmitBar.tsx
@@ -1,10 +1,52 @@
+"use client"
+
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import Editor from "@monaco-editor/react"
+
+interface ProblemsProp{
+    title:string
+}
+
+export async function ProblemSubmitBar({problems}:{problems:ProblemsProp}) {
 
 
+    return (
+        <div className="bg-neutral-800 rounded-2xl p-4">
+            <div>
+            <Tabs>
+                <TabsList className="">
+                    <TabsTrigger value="problem"> Submit </TabsTrigger>
+                    <TabsTrigger value="submissions"> Submission </TabsTrigger>
+                </TabsList>
+                </Tabs>
+            </div>
+            <div>
+                <SubmitProblem prbolem={ problems} />
+            </div>
+        </div>
+    )
+}
 
-export async function ProblemSubmitBar() {
+
+function SubmitProblem({
+    problem
+}: {
+    problem:ProblemsProp
+    }) {
+    
+    
+    
     return (
         <div>
+            <Editor
+                height={"60vh"}
+                theme="vs-dark"
+
+            defaultLanguage="javascript"
             
+            
+            
+            />
         </div>
     )
 }
