61342e24b30095738fe70d3a7a2105eccdcaf9f1
yash gupta <141262414+yashtech00@users.noreply.github.com>
2025-03-12 11:04:28 +0530

    implement GET route for fetching discussions; enhance error handling in POST route

--- apps/web/app/api/discuss/route.ts
+++ apps/web/app/api/discuss/route.ts
@@ -4,35 +4,95 @@ import { authOptions } from "../../../lib/auth-Options";
 import prisma from "../../../db";
 import { discussSchema } from "../../../lib/discuss-schema";
 
+export async function POST(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+  if (!session?.user.id) {
+    return NextResponse.json(
+      {
+        message: "You are not logged In",
+      },
+      {
+        status: 404,
+      }
+    );
+  }
 
-
-export async function POST(req:NextRequest) {
-    const session = await getServerSession(authOptions);
-    if (!session?.user.id) {
-        return NextResponse.json({
-            message:"You are not logged In"
-        }, {
-            status:404
-        })
-    }
-
+  try {
     const discussParsed = discussSchema.safeParse(await req.json());
     if (!discussParsed.success) {
-        return NextResponse.json({
-            message: "Invalid data"
-        }, {
-            status: 400
-        });
+      return NextResponse.json(
+        {
+          message: "Invalid data",
+        },
+        {
+          status: 400,
+        }
+      );
     }
     const discuss = await prisma.discuss.create({
-        data: {
-            title: discussParsed.data.title,
-            description: discussParsed.data.description,
-            upVote: discussParsed.data.upVote,
-            downVote: discussParsed.data.downVote,
-            comment: discussParsed.data.comment,
-            userId:session?.user?.id
-            
-        }
-    })
+      data: {
+        title: discussParsed.data.title,
+        description: discussParsed.data.description,
+        upVote: discussParsed.data.upVote,
+        downVote: discussParsed.data.downVote,
+        comment: discussParsed.data.comment,
+        userId: session?.user?.id,
+      },
+    });
+    return NextResponse.json(
+      {
+        message: "comment created successfully",
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json(
+      {
+        message: "Internal server error",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
+
+export async function GET(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+  if (!session?.user.id) {
+    return NextResponse.json(
+      {
+        message: "You are not logged In",
+      },
+      {
+        status: 404,
+      }
+    );
+  }
+
+  try {
+    const discuss = await prisma.discuss.findMany({});
+    return NextResponse.json(
+      {
+        message: "comment created successfully",
+        discuss,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json(
+      {
+        message: "Internal server error",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
