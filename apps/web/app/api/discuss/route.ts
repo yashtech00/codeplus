8fd1716079cb076cfeb59c5d65f4df59dcab7109
yash gupta <141262414+yashtech00@users.noreply.github.com>
2025-03-12 10:45:26 +0530

    add Discuss model and API route for discussion creation

---
+++ apps/web/app/api/discuss/route.ts
@@ -0,0 +1,24 @@
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "../../../lib/auth-Options";
+import prisma from "../../../db";
+
+
+
+export async function POST(req:NextRequest) {
+    const session = await getServerSession(authOptions);
+    if (!session?.user.id) {
+        return NextResponse.json({
+            message:"You are not logged In"
+        }, {
+            status:404
+        })
+    }
+
+    const 
+    const discuss = await prisma.discuss.create({
+        data: {
+            title:
+        }
+    })
+}
