2f887e15f5f6c1b454d637444a490cc85a844178
yash gupta <141262414+yashtech00@users.noreply.github.com>
2025-03-14 21:43:43 +0530

    update package.json

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,11 @@
-import { AppBar } from "../component/AppBar/Appbar";
+
 import LandingPage from "../component/Landing/LandingPage";
 
  
 export default function Page() {
   return (
     <div>
-      {/* <AppBar/> */}
+     
       <LandingPage/>
     </div>
   );
