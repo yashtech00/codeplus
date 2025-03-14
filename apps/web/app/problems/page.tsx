3fe22e9df18631d5c90066f4c272fd4c52b88d90
yash gupta <141262414+yashtech00@users.noreply.github.com>
2025-03-14 16:32:57 +0530

    fix error

--- apps/web/app/problems/page.tsx
+++ apps/web/app/problems/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { Problems } from "../../component/Problems";
 
-export default function problemList({ searchParams }: { searchParams: { q?: string } }) {
+export default function problemList({ searchParams }: { searchParams: { q: string } }) {
     return (
         <div className="relative ">    
             <Problems searchParams={searchParams} />
